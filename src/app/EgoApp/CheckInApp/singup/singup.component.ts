d75901450262b21d4789c15e4be412a325a958b3
koinu <daniel_ortizpary@hotmail.com>
2022-04-16 13:35:51 -0500

    LINT PASS

--- src/app/EgoApp/CheckInApp/singup/singup.component.ts
+++ src/app/EgoApp/CheckInApp/singup/singup.component.ts
@@ -9,6 +9,7 @@ export class SingupComponent implements OnInit {
 
   constructor() { }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
   }
 
--- src/app/EgoApp/PlayableApp/Steps/result/result.component.ts
+++ src/app/EgoApp/PlayableApp/Steps/result/result.component.ts
@@ -9,6 +9,7 @@ export class ResultComponent implements OnInit {
 
   constructor() { }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
   }
 
