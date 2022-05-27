6ed715c102ef8f950b105dc957a0913ccde488d2
koinu <daniel_ortizpary@hotmail.com>
2022-05-26 23:57:42 -0500

    mejoras

--- src/app/EgoApp/CreateFormsApp/add-question/add-question.component.ts
+++ src/app/EgoApp/CreateFormsApp/add-question/add-question.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CARD_RES_INIT } from './constants/card-res.cosntants';
 import { IRespuestaCard } from './interfaces/respuesta-card.interface';
 
 @Component({
   selector: 'app-add-question',
   templateUrl: './add-question.component.html',
-  styleUrls: ['./add-question.component.scss']
+  styleUrls: ['./add-question.component.scss'],
 })
 export class AddQuestionComponent implements OnInit {
-  public cardRes:Array<IRespuestaCard>;
+  public cardRes: Array<IRespuestaCard>;
   public numRes: number;
-  constructor() { 
-    this.cardRes=new Array<IRespuestaCard>();
-    this.numRes=0;
+  constructor() {
+    this.cardRes = new Array<IRespuestaCard>();
+    this.numRes = 0;
   }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
@@ -19,35 +20,26 @@ export class AddQuestionComponent implements OnInit {
     this.initParameters();
   }
 
-  public initParameters(){
-    this.cardRes=[
-      {
-        resNum: 1,
-        isCorrect: true
-      },
-      {
-        resNum: 2,
-        isCorrect: false
-      },
-    ];
-    this.numRes= this.cardRes.length;
+  public initParameters() {
+    this.cardRes = CARD_RES_INIT();
+    this.actualizarNumRes();
   }
-  public addCardRes(){
-    if(this.numRes<4){
+  public addCardRes() {
+    if (this.numRes < 4) {
       this.cardRes.push({
-        resNum: this.numRes+1,
-        isCorrect: false
-      })
+        resNum: this.numRes + 1,
+        isCorrect: false,
+      });
     }
     this.actualizarNumRes();
   }
-  public delCardRes(){
-    if(this.cardRes.length>2){
+  public delCardRes() {
+    if (this.cardRes.length > 2) {
       this.cardRes.pop();
     }
     this.actualizarNumRes();
   }
-  public actualizarNumRes(){
-    this.numRes= this.cardRes.length;
+  public actualizarNumRes() {
+    this.numRes = this.cardRes.length;
   }
 }
---
+++ src/app/EgoApp/CreateFormsApp/add-question/constants/card-res.cosntants.ts
@@ -0,0 +1,12 @@
+export const CARD_RES_INIT = () => {
+  return [
+    {
+      resNum: 1,
+      isCorrect: true,
+    },
+    {
+      resNum: 2,
+      isCorrect: false,
+    },
+  ];
+};
